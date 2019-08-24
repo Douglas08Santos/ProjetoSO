223d7466ce28f9bf6668c7eb1db9bffb8f20efc9
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-08-24 11:28:10 -0300

    Tentativa de compartilhar variavel entre processos e threads

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import threading
+import multiprocessing
+
 def func(var1, var2):
     print('{}, {}'.format(var1, var2))
+    res.append(var1 + var2)
 
 def unroll(args, func, method, results):
-    # TODO
     if method == 'proc':
-        print('Processos...')
+        for i in args:
+            multiprocessing.Process(target = func, args = (i[0], i[1])).start()
     elif method == 'thre':
-        print('Threads...')
+        for i in args:
+            threading.Thread(target = func, args = (i[0], i[1])).start()
     else:
         print('method incorreto.')
         exit(1)
 
-res = []
-unroll([[0, 1], [0, 2]], func, 'thre', res)
-unroll([[0, 1], [0, 2]], func, 'proc', res)
+matriz = [
+    [1, 2],
+    [3, 4]
+]
+res = multiprocessing.Manager().list()
+unroll(matriz, func, 'thre', res)
+# unroll([[0, 1], [0, 2]], func, 'proc', res)
