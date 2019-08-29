8a64eaab140010ae84cbf7f0022403729b6e822e
Carlos Eduardo <carloseduardofox8@gmail.com>
2019-08-28 22:26:16 -0300

    Adicionada comunicação entre processos na soma de matrizes

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+import os
+import mmap
+import signal
+import struct
+import posix_ipc
 import threading
 
+
 def func(mR, mA, mB, i, j):
-    # print('[{}][{}] = {}, {}'.format(i,j, mA[i][j], mB[i][j]))
-    mR[i][j] = mA[i][j] + mB[i][j]
+    mR.seek((i * len(mA[i]) + j) * 4)
+    mR.write(struct.pack('i', mA[i][j] + mB[i][j]))
 
 def unroll(args, func, method, results):
     if method == 'proc':
-        for i in args:
-            print('...')
-            # multiprocessing.Process(target = func, args = (i[0], i[1])).start()
+        for i in range(0, len(args[0][0])):
+            for j in range(0, len(args[0][1])):
+                pid = os.fork()
+                if pid == 0:
+                    func(results, args[0], args[1], i, j)
+                    exit(0)
     elif method == 'thre':
         for i in range(len(args[0][0])):
             for j in range(len(args[0][1])):
@@ -27,8 +36,13 @@ matrizB = [
     [3, 4]
 ]
 
-matrixR = [[0,0],[0,0]]
-unroll([matrizA, matrizB], func, 'thre', matrixR)
-# unroll([[0, 1], [0, 2]], func, 'proc', res)
+memoria = posix_ipc.SharedMemory('matrizR', flags = posix_ipc.O_CREAT, mode = 0o777, size = 16)
+matrizR = mmap.mmap(memoria.fd, memoria.size)
+memoria.close_fd()
+unroll([matrizA, matrizB], func, 'thre', matrizR)
+unroll([matrizA, matrizB], func, 'proc', matrizR)
 
-print(matrixR)
+print(struct.unpack('iiii', matrizR))
+
+matrizR.close()
+posix_ipc.unlink_shared_memory('matrizR')
